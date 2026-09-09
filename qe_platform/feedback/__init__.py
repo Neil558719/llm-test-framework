fc59ae3ae46707eed7af79ce1be507ba4acba46a
Neil558719 <1410795602@qq.com>
2026-09-09 21:49:27 +0800

    feat: add structured feedback review contracts

--- qe_platform/feedback/__init__.py
+++ qe_platform/feedback/__init__.py
@@ -1,3 +1,7 @@
 from .models import FeedbackInput, FeedbackKind, FeedbackQuery, FeedbackRecord
+from .review import FeedbackReview, PromotionRecord, ReviewAttribution, ReviewPriority, ReviewStatus
 
-__all__ = ["FeedbackInput", "FeedbackKind", "FeedbackQuery", "FeedbackRecord"]
+__all__ = [
+    "FeedbackInput", "FeedbackKind", "FeedbackQuery", "FeedbackRecord",
+    "FeedbackReview", "PromotionRecord", "ReviewAttribution", "ReviewPriority", "ReviewStatus",
+]
